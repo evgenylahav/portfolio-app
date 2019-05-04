448a72803e06910a8d519667915c46ec2640ac59
evgenylahav <evgeny.lahav@gmail.com>
2019-05-04 19:14:06 +0300

    restructuring aboutme, resume and navbar components. added some images and changing the sttyling

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
-import { Cell, Grid } from 'react-mdl';
+import { Container, Col, Image } from 'react-bootstrap';
 import './AboutMe.scss';
 
 import background from '../assets/apple-bg.jpg';
-
+import profilePic from '../assets/Image-1.png';
+import programmingPic from '../assets/programming.jpg';
 
 export interface AboutMeProps {
 
@@ -21,38 +22,68 @@ export class AboutMe extends React.Component<AboutMeProps, AboutMeState> {
 
     public render() {
         return (
-            <div className='aboutme-grid'
-                 >
-                <div style={{backgroundImage: `url(${background})`, backgroundSize: 'cover'}}>
-                    <h3>Discover who am I as a person outside of my day job</h3>
-                    <hr />
-                </div>
-                <div>
-                    <h2>A few facts about myself</h2>
+            <div className='aboutme-grid'>
+                <Image src={background} className='about-header-image' />
+                <h3>Discover who am I as a person outside of my day job</h3>
+            
+                <Container>
+                    <Col xs={12} sm={12} sm-offset={2}>
+                        <Image src={profilePic} roundedCircle className="about-profile-pic" />
+                        <h2>A few facts about myself</h2>
+                        <p>
+                            I am married to Yardena, a school councelor, and one of the smartest people I know, and I am a father of Romi who is 7 now,
+                            and is my biggest pride. I love to travel with my family. We have explored several unforgetable places and hopefully many more
+                            will follow.
+                            I am a sports enthusiast, although, unfortunatelly, as a viewer. I love Tennis and Basketball the most, but during big events
+                            such as the Olympics or the World Cup, I watch everything that there is.
+                            I love food. Both making and eating.
+                            I have a lovely little Yorkshire Terrier named Belle, who is a genuine part of our family, and who never spends a night outside
+                            of our bed.
+                            I love coffee - can't live without it.
+                            I am a fan TV shows, watching quite a few on Netflix and other platforms.
+                         </p>
+                         
+                         <Image src={programmingPic} roundedCircle className="about-profile-pic" />
+                         <h2>Me as a software engineer</h2>
+                         <p>
+                             I made quite a few turn arounds during my proffessional career. First I was in love with image processing. Then with elecro-optics,
+                             specifically with optical design, but now I feel that I found what I was looking for.
+                             Having the experience and a broad
+                         </p>
+                    </Col>
 
-                    <p>
-                        I am married to Yardena, a school councelor, and one of the smartest people I know, and I am a father of Romi who is 7 now,
-                        and is my biggest pride. I love to travel with my family. We have explored several unforgetable places and hopefully many more
-                        will follow.
-                        I am a sports enthusiast, although, unfortunatelly, as a viewer. I love Tennis and Basketball the most, but during big events
-                        such as the Olympics or the World Cup, I watch everything that there is.
-                        I love food. Both making and eating.
-                        I have a lovely little Yorkshire Terrier named Belle, who is a genuine part of our family, and who never spends a night outside
-                        of our bed.
-                        I love coffee - can't live without it.
-                        I am a fan TV shows, watching quite a few on Netflix and other platforms.
-                        </p>
-                    <hr />
-                    <h2>Me as a software engineer</h2>
-
-                    <p>
-                        I made quite a few turn arounds during my proffessional career. First I was in love with image processing. Then with elecro-optics,
-                        specifically with optical design, but now I feel that I found what I was looking for.
-                        Having the experience and a broad
-                        </p>
-                    <hr />
-                </div>
+                </Container>
             </div>
+            //     <div style={{backgroundImage: `url(${background})`, backgroundSize: 'cover'}}>
+            //         <h3>Discover who am I as a person outside of my day job</h3>
+            //         <hr />
+            //     </div>
+            //     <div>
+            //         <h2>A few facts about myself</h2>
+
+            //         <p>
+            //             I am married to Yardena, a school councelor, and one of the smartest people I know, and I am a father of Romi who is 7 now,
+            //             and is my biggest pride. I love to travel with my family. We have explored several unforgetable places and hopefully many more
+            //             will follow.
+            //             I am a sports enthusiast, although, unfortunatelly, as a viewer. I love Tennis and Basketball the most, but during big events
+            //             such as the Olympics or the World Cup, I watch everything that there is.
+            //             I love food. Both making and eating.
+            //             I have a lovely little Yorkshire Terrier named Belle, who is a genuine part of our family, and who never spends a night outside
+            //             of our bed.
+            //             I love coffee - can't live without it.
+            //             I am a fan TV shows, watching quite a few on Netflix and other platforms.
+            //             </p>
+            //         <hr />
+            //         <h2>Me as a software engineer</h2>
+
+            //         <p>
+            //             I made quite a few turn arounds during my proffessional career. First I was in love with image processing. Then with elecro-optics,
+            //             specifically with optical design, but now I feel that I found what I was looking for.
+            //             Having the experience and a broad
+            //             </p>
+            //         <hr />
+            //     </div>
+            // </div>
         );
     }
 }
--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -1,7 +1,9 @@
 import * as React from 'react';
+import { Container, Row, Col, Image } from 'react-bootstrap';
 import { Grid, Cell } from 'react-mdl';
 import { ResumeLine } from './ResumeLine';
 import { Skills } from './Skills';
+import board from '../assets/blackboard-chalk.jpg';
 import im1 from "../assets/Image-1.png";
 import './Resume.scss';
 
@@ -22,72 +24,88 @@ export class Resume extends React.Component<ResumeCompProps, ResumeCompState> {
     public render() {
         return (
             <div>
-                <Grid>
-                    <Cell className='resume-left-col' col={4}>
-                        <div style={{textAlign: 'center'}}>
-                            <img 
-                                src={im1}
-                                alt='avatar'
-                                style={{height: '200px', width: 'auto'}}
-                            />   
-                        </div>
-                        <h2 style={{textAlign: 'center'}}>Evgeny Lahav</h2>
-                        <h4 style={{color: 'grey', textAlign: 'center'}}>Software Engineer</h4>                 
-                        <hr/>
-                        <p style={{paddingTop: '5px'}}>An experienced and enthuziastic SW engineer, specializing in Python, JavaScript and React</p>
-                        <hr/>
-                        <h5>email</h5>
-                        <p>evgeny.lahav@gmail.com</p>
-                        <h5>mobile</h5>
-                        <p>+972-(0)5444-86872</p>
-                        <h5>website</h5>
-                        <p>...</p>
-                        <hr/>
-                    </Cell>
-                    <Cell className='resume-right-col'  col={8}>
-                        <h2>Experience</h2>
-                        <ResumeLine 
-                            startYear='2013' 
-                            endYear='current' 
-                            placeName='Applied Materials'
-                            description='Software engineer: ...'
-                        />
-                        <ResumeLine 
-                            startYear='2007' 
-                            endYear='2013' 
-                            placeName='Applied Materials'
-                            description='System engineer: ...'
-                        />
-                        <ResumeLine 
-                            startYear='2006' 
-                            endYear='2007' 
-                            placeName='Applied Materials'
-                            description='Software QA engineer: ...'
-                        />
-                        <hr/>
-                        <h2>Education</h2>
-                        <ResumeLine 
-                            startYear='2014' 
-                            endYear='2018' 
-                            placeName='Ben-Gurion University'
-                            description='M.Sc in Electro-optical engineering. Grades average of 90. Thesis subject...'
-                        />
-                        <ResumeLine 
-                            startYear='2001' 
-                            endYear='2005' 
-                            placeName='Ben-Gurion University'
-                            description='B.Sc in Bio-Medical Engineering. Grades average of 85.'
-                        />
-                        <hr/>
-                        <h2>Skills</h2>
-                        <Skills skill='Python' skillLevel={70}/>
-                        <Skills skill='JavaScript' skillLevel={50}/>
-                        <Skills skill='React' skillLevel={40}/>
-                        <Skills skill='Scientific Programming' skillLevel={80}/>
-                        <Skills skill='Clean Code' skillLevel={60}/>
-                    </Cell>
-                </Grid>
+                <Image src={board} className="resume-header-image" />
+                <Container className='resume-container'>
+                    <h3>My Resume</h3>
+                    <Row>
+                        <Col xs={12} sm={4}>
+                            <div style={{textAlign: 'center'}}>
+                                <Image src={im1} roundedCircle className="resume-profile-pic" />
+
+                            </div>
+                            <h2 style={{textAlign: 'center'}}>Evgeny Lahav</h2>
+                            <h4 style={{color: 'grey', textAlign: 'center'}}>Software Engineer</h4>                 
+                            <hr/>
+                            <p style={{paddingTop: '5px'}}>An experienced and enthuziastic SW engineer, specializing in Python, JavaScript and React</p>
+                            <hr/>
+                            <h5>email</h5>
+                            <p>evgeny.lahav@gmail.com</p>
+                            <h5>mobile</h5>
+                            <p>+972-(0)5444-86872</p>
+                            <h5>website</h5>
+                            <p>...</p>
+                            <hr/>
+                        </Col>
+                        <Col sm={1} className='verticalLine' />
+                        
+                        <Col xs={12} sm={7}>
+                            <h2>Experience</h2>
+                            <ResumeLine 
+                                startYear='2013' 
+                                endYear='current' 
+                                placeName='Applied Materials'
+                                description='Software engineer: ...'
+                            />
+                            <ResumeLine 
+                                startYear='2007' 
+                                endYear='2013' 
+                                placeName='Applied Materials'
+                                description='System engineer: ...'
+                            />
+                            <ResumeLine 
+                                startYear='2006' 
+                                endYear='2007' 
+                                placeName='Applied Materials'
+                                description='Software QA engineer: ...'
+                            />
+                            <hr/>
+                            <h2>Education</h2>
+                            <ResumeLine 
+                                startYear='2014' 
+                                endYear='2018' 
+                                placeName='Ben-Gurion University'
+                                description='M.Sc in Electro-optical engineering. Grades average of 90. Thesis subject...'
+                            />
+                            <ResumeLine 
+                                startYear='2001' 
+                                endYear='2005' 
+                                placeName='Ben-Gurion University'
+                                description='B.Sc in Bio-Medical Engineering. Grades average of 85.'
+                            />
+                            <hr/>
+                            <h2>Skills</h2>
+                            <Skills skill='Python' skillLevel={70}/>
+                            <Skills skill='JavaScript' skillLevel={50}/>
+                            <Skills skill='React' skillLevel={40}/>
+                            <Skills skill='Scientific Programming' skillLevel={80}/>
+                            <Skills skill='Clean Code' skillLevel={60}/>
+                        </Col>
+
+                    </Row>
+
+                </Container>
             </div>
         );
     }
 }
+                    /* <Cell className='resume-left-col' col={4}>
+                        
+                    </Cell>
+                    <Cell className='resume-right-col'  col={8}>
+                        
+                    </Cell>
+                </Grid> */
+            // </div>
+    //     );
+    // }
+// }
