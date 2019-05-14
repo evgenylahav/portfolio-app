2ebdcfaa136efcdf64197717760d1c9954f3f2c0
evgenylahav <evgeny.lahav@gmail.com>
2019-05-15 00:58:44 +0300

    landing page routing fix

--- src/components/pages/LandingPage.tsx
+++ src/components/pages/LandingPage.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { Container, Col, Row, Image } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import im1 from '../../assets/me_young.jpeg';
 // import bg_pic from '../../assets/bg_bricks.jpg';
 import './LandingPage.scss';
@@ -21,7 +22,10 @@ export class LandingPage extends React.Component<LandingPageProps, LandingPageSt
                         <Image src={im1} roundedCircle className="profile-pic" />
                         <h2 className='me'>Evgeny Lahav</h2>
                         <h4>Software engineer, practicing with Python, JavaScript and React</h4>
-                        <h4>Check out my <a href='/portfolio-app/resume'>resume</a>, <a href='/portfolio-app/aboutme'>about me</a> and my <a href='/portfolio-app/blog'>blog</a> and be in touch</h4>
+                        <h4>Check out my <Link to="/portfolio-app/resume">resume</Link>,  
+                                         <Link to='/portfolio-app/aboutme'> about me</Link> and my 
+                                         <Link to='/portfolio-app/blog'> blog</Link> and be in touch</h4>
+                        {/* <h4>Check out my <a href='/portfolio-app/resume'>resume</a>, <a href='/portfolio-app/aboutme'>about me</a> and my <a href='/portfolio-app/blog'>blog</a> and be in touch</h4> */}
                     </Col>
                 </Row>
                 {/* <Social /> */}
